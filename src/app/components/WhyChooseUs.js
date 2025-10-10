bfd2a875d59d5ecc38097b9eaecc994ec019b40c
RAVI08RR <ravisoni08rrrr@gmail.com>
2025-10-10 18:14:58 +0530

    api updated on contact form

--- src/app/components/WhyChooseUs.js
+++ src/app/components/WhyChooseUs.js
@@ -1,24 +1,160 @@
 
 
+// SVG Icon Components
+const HomeIcon = () => (
+<svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint0_linear_81_419)"/>
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint1_linear_81_419)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint2_linear_81_419)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint3_linear_81_419)"/>
+<path d="M28.8941 36.1057C30.8858 38.0974 34.115 38.0974 36.1066 36.1057C36.3514 35.861 36.566 35.5975 36.7506 35.32C37.6644 36.694 39.2266 37.5995 41.0004 37.5995C42.7744 37.5995 44.3368 36.6938 45.2505 35.3195C45.4352 35.5972 45.65 35.8608 45.8949 36.1058C47.8866 38.0974 51.1157 38.0974 53.1074 36.1058C55.0991 34.1141 55.0991 30.8849 53.1074 28.8933L52.6099 28.3957C51.9722 27.7581 51.1074 27.3999 50.2057 27.3999H31.7958C30.894 27.3999 30.0292 27.7581 29.3916 28.3957L28.8941 28.8932C26.9025 30.8849 26.9025 34.114 28.8941 36.1057Z" fill="#C08735"/>
+<path d="M29.1004 39.3547C31.528 40.5591 34.4621 40.3946 36.7514 38.8614C37.9659 39.674 39.4274 40.1495 41.0004 40.1495C42.5735 40.1495 44.0352 39.6739 45.2498 38.8612C47.5389 40.3945 50.4728 40.5591 52.9004 39.3551V52.0499H53.3254C54.0296 52.0499 54.6004 52.6207 54.6004 53.3249C54.6004 54.0291 54.0296 54.5999 53.3254 54.5999H45.6754C44.9712 54.5999 44.4004 54.0291 44.4004 53.3249V47.3749C44.4004 46.6707 43.8296 46.0999 43.1254 46.0999H38.8754C38.1712 46.0999 37.6004 46.6707 37.6004 47.3749V53.3249C37.6004 54.0291 37.0296 54.5999 36.3254 54.5999H28.6754C27.9712 54.5999 27.4004 54.0291 27.4004 53.3249C27.4004 52.6207 27.9712 52.0499 28.6754 52.0499H29.1004V39.3547Z" fill="#C08735"/>
+<defs>
+<linearGradient id="paint0_linear_81_419" x1="6.75" y1="86.5" x2="85.75" y2="-5.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint1_linear_81_419" x1="81.75" y1="-10.5" x2="6.75" y2="82" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint2_linear_81_419" x1="3.25" y1="13.5" x2="71.75" y2="72" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint3_linear_81_419" x1="84.25" y1="86" x2="12.25" y2="35" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>
+
+);
+
+const ValueIcon = () => (
+<svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint0_linear_81_435)"/>
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint1_linear_81_435)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint2_linear_81_435)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint3_linear_81_435)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M25.7002 28.6749C25.7002 27.9707 26.271 27.3999 26.9752 27.3999H44.8252C45.5294 27.3999 46.1002 27.9707 46.1002 28.6749C46.1002 29.3791 45.5294 29.9499 44.8252 29.9499H44.4002V53.3249C44.4002 54.0291 43.8294 54.5999 43.1252 54.5999H40.5752C39.871 54.5999 39.3002 54.0291 39.3002 53.3249V49.0749C39.3002 48.3707 38.7294 47.7999 38.0252 47.7999H33.7752C33.071 47.7999 32.5002 48.3707 32.5002 49.0749V53.3249C32.5002 54.0291 31.9294 54.5999 31.2252 54.5999H26.9752C26.271 54.5999 25.7002 54.0291 25.7002 53.3249C25.7002 52.6207 26.271 52.0499 26.9752 52.0499H27.4002V29.9499H26.9752C26.271 29.9499 25.7002 29.3791 25.7002 28.6749ZM30.8002 33.3499C30.8002 32.8805 31.1808 32.4999 31.6502 32.4999H33.3502C33.8196 32.4999 34.2002 32.8805 34.2002 33.3499V35.0499C34.2002 35.5193 33.8196 35.8999 33.3502 35.8999H31.6502C31.1808 35.8999 30.8002 35.5193 30.8002 35.0499V33.3499ZM31.6502 39.2999C31.1808 39.2999 30.8002 39.6805 30.8002 40.1499V41.8499C30.8002 42.3193 31.1808 42.6999 31.6502 42.6999H33.3502C33.8196 42.6999 34.2002 42.3193 34.2002 41.8499V40.1499C34.2002 39.6805 33.8196 39.2999 33.3502 39.2999H31.6502ZM37.6002 33.3499C37.6002 32.8805 37.9808 32.4999 38.4502 32.4999H40.1502C40.6196 32.4999 41.0002 32.8805 41.0002 33.3499V35.0499C41.0002 35.5193 40.6196 35.8999 40.1502 35.8999H38.4502C37.9808 35.8999 37.6002 35.5193 37.6002 35.0499V33.3499ZM38.4502 39.2999C37.9808 39.2999 37.6002 39.6805 37.6002 40.1499V41.8499C37.6002 42.3193 37.9808 42.6999 38.4502 42.6999H40.1502C40.6196 42.6999 41.0002 42.3193 41.0002 41.8499V40.1499C41.0002 39.6805 40.6196 39.2999 40.1502 39.2999H38.4502Z" fill="#C08735"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M48.2252 34.1999C47.521 34.1999 46.9502 34.7707 46.9502 35.4749V52.8999C46.9502 53.8388 47.7113 54.5999 48.6502 54.5999H55.0252C55.7294 54.5999 56.3002 54.0291 56.3002 53.3249C56.3002 52.6207 55.7294 52.0499 55.0252 52.0499H54.6002V36.7499H55.0252C55.7294 36.7499 56.3002 36.1791 56.3002 35.4749C56.3002 34.7707 55.7294 34.1999 55.0252 34.1999H48.2252ZM49.0752 40.1499C49.0752 39.6805 49.4558 39.2999 49.9252 39.2999H51.6252C52.0946 39.2999 52.4752 39.6805 52.4752 40.1499V41.8499C52.4752 42.3193 52.0946 42.6999 51.6252 42.6999H49.9252C49.4558 42.6999 49.0752 42.3193 49.0752 41.8499V40.1499ZM49.9252 46.0999C49.4558 46.0999 49.0752 46.4805 49.0752 46.9499V48.6499C49.0752 49.1193 49.4558 49.4999 49.9252 49.4999H51.6252C52.0946 49.4999 52.4752 49.1193 52.4752 48.6499V46.9499C52.4752 46.4805 52.0946 46.0999 51.6252 46.0999H49.9252Z" fill="#C08735"/>
+<defs>
+<linearGradient id="paint0_linear_81_435" x1="6.75" y1="86.5" x2="85.75" y2="-5.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint1_linear_81_435" x1="81.75" y1="-10.5" x2="6.75" y2="82" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint2_linear_81_435" x1="3.25" y1="13.5" x2="71.75" y2="72" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint3_linear_81_435" x1="84.25" y1="86" x2="12.25" y2="35" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>
+
+);
+
+const ManagementIcon = () => (
+<svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint0_linear_81_443)"/>
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint1_linear_81_443)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint2_linear_81_443)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint3_linear_81_443)"/>
+<path d="M41 27.1875C41.5868 27.1875 42.0625 27.6632 42.0625 28.25V31.4375C42.0625 32.0243 41.5868 32.5 41 32.5C40.4132 32.5 39.9375 32.0243 39.9375 31.4375V28.25C39.9375 27.6632 40.4132 27.1875 41 27.1875Z" fill="#C08735"/>
+<path d="M34.625 41C34.625 37.4792 37.4792 34.625 41 34.625C44.5208 34.625 47.375 37.4792 47.375 41C47.375 44.5208 44.5208 47.375 41 47.375C37.4792 47.375 34.625 44.5208 34.625 41Z" fill="#C08735"/>
+<path d="M50.767 32.7356C51.1819 32.3207 51.1819 31.648 50.767 31.233C50.3521 30.8181 49.6793 30.8181 49.2644 31.233L47.0105 33.4869C46.5955 33.9019 46.5955 34.5746 47.0105 34.9895C47.4254 35.4045 48.0982 35.4045 48.5131 34.9895L50.767 32.7356Z" fill="#C08735"/>
+<path d="M54.8125 41C54.8125 41.5868 54.3368 42.0625 53.75 42.0625H50.5625C49.9757 42.0625 49.5 41.5868 49.5 41C49.5 40.4132 49.9757 39.9375 50.5625 39.9375H53.75C54.3368 39.9375 54.8125 40.4132 54.8125 41Z" fill="#C08735"/>
+<path d="M49.2642 50.7669C49.6792 51.1818 50.3519 51.1818 50.7668 50.7669C51.1818 50.3519 51.1818 49.6792 50.7668 49.2643L48.5129 47.0104C48.098 46.5954 47.4253 46.5954 47.0103 47.0104C46.5954 47.4253 46.5954 48.098 47.0103 48.513L49.2642 50.7669Z" fill="#C08735"/>
+<path d="M41 49.5C41.5868 49.5 42.0625 49.9757 42.0625 50.5625V53.75C42.0625 54.3368 41.5868 54.8125 41 54.8125C40.4132 54.8125 39.9375 54.3368 39.9375 53.75V50.5625C39.9375 49.9757 40.4132 49.5 41 49.5Z" fill="#C08735"/>
+<path d="M34.9898 48.513C35.4047 48.098 35.4047 47.4253 34.9898 47.0104C34.5749 46.5954 33.9021 46.5954 33.4872 47.0104L31.2333 49.2643C30.8184 49.6792 30.8184 50.3519 31.2333 50.7669C31.6482 51.1818 32.321 51.1818 32.7359 50.7669L34.9898 48.513Z" fill="#C08735"/>
+<path d="M32.5 41C32.5 41.5868 32.0243 42.0625 31.4375 42.0625H28.25C27.6632 42.0625 27.1875 41.5868 27.1875 41C27.1875 40.4132 27.6632 39.9375 28.25 39.9375H31.4375C32.0243 39.9375 32.5 40.4132 32.5 41Z" fill="#C08735"/>
+<path d="M33.487 34.9895C33.902 35.4045 34.5747 35.4045 34.9896 34.9895C35.4046 34.5746 35.4046 33.9019 34.9896 33.4869L32.7357 31.233C32.3208 30.8181 31.6481 30.8181 31.2331 31.233C30.8182 31.648 30.8182 32.3207 31.2331 32.7356L33.487 34.9895Z" fill="#C08735"/>
+<defs>
+<linearGradient id="paint0_linear_81_443" x1="6.75" y1="86.5" x2="85.75" y2="-5.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint1_linear_81_443" x1="81.75" y1="-10.5" x2="6.75" y2="82" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint2_linear_81_443" x1="3.25" y1="13.5" x2="71.75" y2="72" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint3_linear_81_443" x1="84.25" y1="86" x2="12.25" y2="35" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>
+
+);
+
+const InvestmentIcon = () => (
+<svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint0_linear_81_427)"/>
+<rect x="0.5" y="0.5" width="81" height="81" rx="40.5" stroke="url(#paint1_linear_81_427)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint2_linear_81_427)"/>
+<rect x="10.5" y="10.5" width="61" height="61" rx="30.5" stroke="url(#paint3_linear_81_427)"/>
+<path d="M41 34.625C39.2396 34.625 37.8125 36.0521 37.8125 37.8125C37.8125 39.5729 39.2396 41 41 41C42.7604 41 44.1875 39.5729 44.1875 37.8125C44.1875 36.0521 42.7604 34.625 41 34.625Z" fill="#C08735"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M26.125 30.9062C26.125 29.4392 27.3142 28.25 28.7812 28.25H53.2188C54.6858 28.25 55.875 29.4392 55.875 30.9062V44.7188C55.875 46.1858 54.6858 47.375 53.2188 47.375H28.7812C27.3142 47.375 26.125 46.1858 26.125 44.7188V30.9062ZM35.6875 37.8125C35.6875 34.8785 38.066 32.5 41 32.5C43.934 32.5 46.3125 34.8785 46.3125 37.8125C46.3125 40.7465 43.934 43.125 41 43.125C38.066 43.125 35.6875 40.7465 35.6875 37.8125ZM50.5625 36.75C49.9757 36.75 49.5 37.2257 49.5 37.8125V37.8231C49.5 38.4099 49.9757 38.8856 50.5625 38.8856H50.5731C51.1599 38.8856 51.6356 38.4099 51.6356 37.8231V37.8125C51.6356 37.2257 51.1599 36.75 50.5731 36.75H50.5625ZM30.375 37.8125C30.375 37.2257 30.8507 36.75 31.4375 36.75H31.4481C32.0349 36.75 32.5106 37.2257 32.5106 37.8125V37.8231C32.5106 38.4099 32.0349 38.8856 31.4481 38.8856H31.4375C30.8507 38.8856 30.375 38.4099 30.375 37.8231V37.8125Z" fill="#C08735"/>
+<path d="M27.1875 49.5C26.6007 49.5 26.125 49.9757 26.125 50.5625C26.125 51.1493 26.6007 51.625 27.1875 51.625C34.8376 51.625 42.247 52.6481 49.2872 54.5644C50.9738 55.0235 52.6875 53.7738 52.6875 51.9864V50.5625C52.6875 49.9757 52.2118 49.5 51.625 49.5H27.1875Z" fill="#C08735"/>
+<defs>
+<linearGradient id="paint0_linear_81_427" x1="6.75" y1="86.5" x2="85.75" y2="-5.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint1_linear_81_427" x1="81.75" y1="-10.5" x2="6.75" y2="82" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint2_linear_81_427" x1="3.25" y1="13.5" x2="71.75" y2="72" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.323723" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint3_linear_81_427" x1="84.25" y1="86" x2="12.25" y2="35" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C08735"/>
+<stop offset="0.576615" stop-color="#C08735" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>
+
+);
+
+const ArrowIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 17L17 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M7 7H17V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
+
 export default function WhyChooseUs() {
   const features = [
     {
-      iconSrc: "/images/icon-mr-4.svg",
+      icon: <HomeIcon />,
       title: "Find Your Dream Home",
       description: "Discover properties tailored to your needs"
     },
     {
-      iconSrc: "/images/icon-mr-2.svg",
+      icon: <ValueIcon />,
       title: "Unlock Property Value",
       description: "Maximize your investment potential"
     },
     {
-      iconSrc: "/images/icon-mr-3.svg",
+      icon: <ManagementIcon />,
       title: "Effortless Property Management",
       description: "Streamlined solutions for your properties"
     },
     {
-      iconSrc: "/images/icon-mr-1.svg",
+      icon: <InvestmentIcon />,
       title: "Smart Investments. Informed Decisions",
       description: "Data-driven insights for better choices"
     }
@@ -45,24 +181,14 @@ export default function WhyChooseUs() {
               <div className="relative bg-black hover:border-[#c3b5b5] rounded-lg p-8 transition-all duration-300 hover:bg-black/80 h-full border-[#262626] border-[0.8px]">
                 {/* Arrow Icon */}
                 <div className="absolute top-6 right-6 text-gray-600 group-hover:text-amber-600 transition-colors">
-                  <img 
-                    src="/images/arrow-icon.svg" 
-                    alt="Arrow"
-                    width={20}
-                    height={20}
-                    className="w-5 h-5"
-                  />
+                  <ArrowIcon />
                 </div>
 
                 {/* Icon */}
                 <div className="mb-8 m-auto flex justify-center items-center">
-                  <img 
-                    src={feature.iconSrc}
-                    alt={feature.title}
-                    width={64}
-                    height={64}
-                    className="w-[4rem] h-[4rem]"
-                  />
+                  <div className="w-16 h-16 flex items-center justify-center">
+                    {feature.icon}
+                  </div>
                 </div>
 
                 {/* Title */}
