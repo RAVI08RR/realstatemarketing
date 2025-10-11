c558e2653d002f0640594c51b39325079ff497c1
RAVI08RR <ravisoni08rrrr@gmail.com>
2025-10-11 12:56:46 +0530

    api updated on contact form

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -208,7 +208,7 @@ export default function HeroSection() {
               </div>
 
               {/* Phone and Preferred Location Row */}
-              <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+              <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-4">
               
                 <div className="floating-input">
                   <input
